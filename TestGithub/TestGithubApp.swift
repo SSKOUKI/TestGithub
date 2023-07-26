94202f16fefa053afee6ce7e468a5f9519db1937
Samia <koukii.samia@gmail.com>
2023-07-26 22:53:26 +0400

    Initial Commit

---
+++ TestGithub/TestGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubApp.swift
+//  TestGithub
+//
+//  Created by APPLE on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
